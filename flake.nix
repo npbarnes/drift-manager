08f27aafba1796225828c8cb130c50e22c9d38b9
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-03-31 23:13:18 -0500

    Surprisingly, we're not using home-manager.

--- flake.nix
+++ flake.nix
@@ -3,17 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     {
       self,
-      nixpkgs,
-      home-manager,
+      nixpkgs
     }:
     let
       system = "x86_64-linux";
