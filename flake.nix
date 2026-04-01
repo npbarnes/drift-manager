5972eb563b67d29bd423c224cb43012ce8c6d644
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-03-31 21:55:31 -0500

    Add shfmt check

--- flake.nix
+++ flake.nix
@@ -45,6 +45,19 @@
               find . -name '*.nix' -print0 | xargs -0 nixfmt --check
               touch "$out"
             '';
+
+        shfmt =
+          pkgs.runCommand "check-shfmt"
+            {
+              nativeBuildInputs = [ pkgs.shfmt ];
+              src = self;
+            }
+            ''
+              cd "$src"
+              echo "Checking Bash formatting..."
+              find . -name '*.sh' -print0 | xargs -0 shfmt --indent 4 --diff
+              touch "$out"
+            '';
       };
     };
 }
